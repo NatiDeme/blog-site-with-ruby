b258f6876b719b5460d416150b6d12a160bd7330
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-09-14 18:51:48 +0300

    Add migration file for comments table

---
+++ db/migrate/20220914154050_create_comment.rb
@@ -0,0 +1,11 @@
+class CreateComment < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.timestamp :UpdatedAt
+      t.timestamp :CreatedAt
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220914154639_remove_created_at_from_comments.rb
@@ -0,0 +1,6 @@
+class RemoveCreatedAtFromComments < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :comments, :created_at, :timestamp
+    remove_column :comments, :updated_at, :timestamp
+  end
+end
