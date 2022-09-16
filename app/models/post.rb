6946d088ff8f14657f4e67f41585a5c34844b660
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-09-16 12:28:53 +0300

    Add unit test and validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  
   has_many :likes, foreign_key: 'post_id'
   has_many :comments, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User'
