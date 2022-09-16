31f222169b13b6376799182d8ba2d1ec7b32a53b
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-09-16 10:55:09 +0300

    Add methods to codebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
- has_many :posts, foriegn_key: 'author_id'
- has_many :comments, foriegn_key: 'author_id' 
- has_many :likes, foriegn_key: 'author_id'
+  has_many :posts, foriegn_key: 'author_id'
+  has_many :comments, foriegn_key: 'author_id'
+  has_many :likes, foriegn_key: 'author_id'
+
+  def recent_posts
+    posts.limit(3).order(created_at: :desc)
+  end
 end
