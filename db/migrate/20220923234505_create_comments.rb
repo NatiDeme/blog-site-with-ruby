2f9c33f6c13585f04c0dc01813278093fe2067d1
NatiDeme <demelashnatnael@gmail.com>
2022-09-24 02:48:40 +0300

    Add fix to migration files

---
+++ db/migrate/20220923234505_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.text :Text
+
+      t.timestamps
+    end
+
+    add_reference(:comments, :author, foreign_key:{to_table: :users}, index: true)
+    add_reference(:comments, :post, index: true)
+  end
+end
---
+++ db/migrate/20220923234537_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :Title
+      t.text :Text
+      t.integer :CommentsCounter
+      t.integer :LikesCounter
+
+      t.timestamps
+    end
+
+    add_reference(:posts, :author, foreign_key:{to_table: :users}, index: true)
+
+  end
+end
