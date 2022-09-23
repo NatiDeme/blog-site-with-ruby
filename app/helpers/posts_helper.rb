56f6e8c7816c071fb33adae26410a53e56e170ae
NatiDeme <demelashnatnael@gmail.com>
2022-09-23 13:52:35 +0300

    Add view for users root page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+    def num_of_comment(post)
+    post.CommentsCounter.present? ? post.CommentsCounter : 0
+  end
+
+  def num_of_likes(post)
+    post.LikesCounter.present? ? post.LikesCounter : 0
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+    def num_of_post(user)
+    user.PostCounter.present? ? user.PostCounter : 0
+  end
 end
