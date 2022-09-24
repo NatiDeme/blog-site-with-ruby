12b5aa77aaa0b4b91652982fe84570816c6b6fd8
NatiDeme <demelashnatnael@gmail.com>
2022-09-24 04:35:46 +0300

    Add minor edits on codebase

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
-    @comments = @post.comments
+    @comments = Comment.where(post_id: params[:id])
   end
 end
