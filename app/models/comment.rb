b0110082d163c5aee1611da6f01b352c66a9ce62
NatiDeme <demelashnatnael@gmail.com>
2022-09-24 03:00:19 +0300

    Add user/show view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.increment!(:CommentsCounter)
   end
 end
