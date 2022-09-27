2e54588ddca43b3ce149e44b19fb3bcbbc5af6f7
NatiDeme <demelashnatnael@gmail.com>
2022-09-27 16:09:51 +0300

    Add fix to unwanted migration files

--- db/migrate/20220914160409_create_like.rb
+++
@@ -1,9 +0,0 @@
-# migration class
-class CreateLike < ActiveRecord::Migration[7.0]
-  def change
-    create_table :likes do |t|
-      t.timestamp :UpdatedAt
-      t.timestamp :CreatedAt
-    end
-  end
-end
