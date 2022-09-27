f1b8637ad03b1e30bb503bfc37478ad97d174a51
NatiDeme <demelashnatnael@gmail.com>
2022-09-27 11:53:09 +0300

    Add test for user index page

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'users/index.html.erb', type: :feature do
+  before :each do
+    @user = User.first
+  end
   describe 'index page' do
     it 'shows the Number of posts' do
       visit root_path
       expect(page).to have_content('Number of posts')
     end
+    it 'shows the list of users' do
+      visit root_path
+      expect(page).to have_content('Lilly')
+      expect(page).to have_content('Tom')
+    end
+    it 'shows the user picture' do
+      visit root_path
+      expect(page).to have_css('img')
+  end
+  it 'Redirects to show page' do
+      visit root_path
+      click_link "Tom"
+      expect(page).to have_content('Tom')
+  end
   end
 end
