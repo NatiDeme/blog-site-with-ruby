b81b025d266f95879734877db300b34619716f24
NatiDeme <demelashnatnael@gmail.com>
2022-09-27 15:50:24 +0300

    Add integration spec for posts/index page

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'posts/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'posts/index.html.erb', type: :feature do
+  before (:each) do
+    @user1 = User.first
+    visit user_posts_path(@user1.id)
+  end
+  describe 'Posts index page' do
+    it 'shows the user picture' do
+      expect(page).to have_css('img')
+  end
+  it 'shows the list of users' do
+      expect(page).to have_content('Tom')
+    end
+      it "show user's first 3 posts." do
+      expect(page).to have_content 'Hello'
+      expect(page).to have_content 'test'
+      expect(page).to have_content 'test'
+    end
+      it "show user's first 3 posts." do
+      expect(page).to have_content 'This is my first post'
+      expect(page).to have_content 'this is test'
+      expect(page).to have_content 'this is test'
+    end
+    it 'shows the Number of Likes' do
+      expect(page).to have_content('Likes')
+    end
+    it 'shows Pagination' do
+      expect(page).to have_content('Pagination')
+    end
+  end
 end
