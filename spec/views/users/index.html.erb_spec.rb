e4ae25595231d049f55ce5e0ba48429486c49b42
NatiDeme <demelashnatnael@gmail.com>
2022-09-27 05:49:19 +0300

    Add dependencies and fix profile pictures

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'users/index.html.erb', type: :feature do
+  describe 'index page' do
+    it 'shows the Number of posts' do
+      visit root_path
+      expect(page).to have_content('Number of posts')
+    end
+  end
 end
